7e049c891cdab3793b25655ae4b4441d566891ab
Guilherme Jesus Sales <guijsalesdg@outlook.com>
2022-01-26 14:00:26 -0300

    adicionado menu mobile com funcao de outsideclick e animacao dos links

---
+++ js/modules/menu-mobile.js
@@ -0,0 +1,58 @@
+export default class MenuMobile {
+  constructor(mobileMenu, ulMenu, liMenu) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.ulMenu = document.querySelector(ulMenu);
+    this.liMenu = document.querySelectorAll(liMenu);
+    this.activeClass = 'active';
+    this.html = document.documentElement;
+    this.outside = 'outside';
+
+    this.addMenuClass = this.addMenuClass.bind(this);
+    this.handleOutsideClick = this.handleOutsideClick.bind(this);
+  }
+
+  addMenuClass() {
+    this.mobileMenu.classList.add(this.activeClass);
+    this.ulMenu.classList.add(this.activeClass);
+    this.outsideClick(this.ulMenu);
+    this.animateLinks();
+  }
+
+  animateLinks() {
+    this.liMenu.forEach((links, index) => {
+      links.style.animation
+        ? (links.style.animation = '')
+        : (links.style.animation = `fadeSlide .5s ease forwards ${index / 7 + 0.3}s`);
+    });
+  }
+
+  outsideClick(element) {
+    if (!element.hasAttribute(this.outside)) {
+      element.setAttribute(this.outside, '');
+      setTimeout(() => {
+        this.html.addEventListener('click', this.handleOutsideClick);
+      });
+    }
+  }
+
+  handleOutsideClick(event) {
+    if (this.ulMenu !== event.target) {
+      this.html.removeEventListener('click', this.handleOutsideClick);
+      this.ulMenu.removeAttribute(this.outside);
+      this.mobileMenu.classList.remove(this.activeClass);
+      this.ulMenu.classList.remove(this.activeClass);
+      this.liMenu.forEach(link => {
+        link.style.animation = '';
+      });
+    }
+  }
+
+  addEventMenu() {
+    this.mobileMenu.addEventListener('click', this.addMenuClass);
+  }
+
+  init() {
+    this.addEventMenu();
+    return this;
+  }
+}
---
+++ js/script.js
@@ -0,0 +1,5 @@
+import MenuMobile from './modules/menu-mobile.js';
+
+const mobileMenu = new MenuMobile('.mobile-menu', '.menu-nav ul', '.menu-nav li');
+
+mobileMenu.init();
