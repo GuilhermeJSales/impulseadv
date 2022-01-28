03fa16d121e5aaa63600448daddac2247ec45f15
Guilherme Jesus Sales <guijsalesdg@outlook.com>
2022-01-28 17:49:08 -0300

    adicionado evento de smooth scroll nos links internos do site

---
+++ js/modules/smooth-scroll.js
@@ -0,0 +1,27 @@
+export default class SmoothScroll {
+  constructor(links) {
+    this.sections = document.querySelectorAll(links);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    this.sections = document.querySelector(href);
+    this.sections.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  }
+
+  addEventScroll() {
+    this.sections.forEach((links) => {
+      links.addEventListener('click', this.handleClick);
+    });
+  }
+
+  init() {
+    this.addEventScroll();
+  }
+}
--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 import MenuMobile from './modules/menu-mobile.js';
+import SmoothScroll from './modules/smooth-scroll.js';
 
 const mobileMenu = new MenuMobile('[data-menu="button"]', '[data-menu = "nav"]', '[data-menu = "nav"] li');
 
 mobileMenu.init();
+
+const smoothScroll = new SmoothScroll('a[href^="#"]');
+smoothScroll.init();
