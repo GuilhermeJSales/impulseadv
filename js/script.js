a5e70465a93c04a97ed3bec57263e43eb1fedb53
Guilherme Jesus Sales <guijsalesdg@outlook.com>
2022-02-16 23:52:39 -0300

    adicionado slide da equipe, com botões next e prev

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ import SmoothScroll from './modules/smooth-scroll.js';
 import AnimaNumeros from './modules/anima-numeros.js';
 import Accordion from './modules/accordion.js';
 import AnimationScroll from './modules/scroll-animation.js';
+import SlideNav from './modules/slide.js';
 
 const mobileMenu = new MenuMobile('[data-menu="button"]', '[data-menu = "nav"]', '[data-menu = "nav"] li');
 mobileMenu.init();
@@ -18,3 +19,7 @@ accordionList.init();
 
 const scrollAnimation = new AnimationScroll('[data-scroll]', '[data-index]');
 scrollAnimation.init();
+
+const slide = new SlideNav('.slide', '.slide-wrapper');
+slide.init();
+slide.addArrow('.prev', '.next');
